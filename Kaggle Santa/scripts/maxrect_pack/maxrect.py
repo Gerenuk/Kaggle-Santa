c200f0b3177976f28d15a69be1991a472b7185f1
Anton <the.gerenuk@gmail.com>
2013-12-14 21:01:50 +0100

    Random Maxrect test passed

--- Kaggle Santa/scripts/maxrect_pack/maxrect.py
+++ Kaggle Santa/scripts/maxrect_pack/maxrect.py
@@ -1,20 +1,23 @@
-from maxrect.plotrect import plotrects
-from maxrect.rect import Rectangle
-
-
-MAXRECT_PLOT_OPTIONS={"fc":"yellow","ec":"black", "alpha":0.1}
+from maxrect_pack.plotrect import plotrects
+from maxrect_pack.rect import Rectangle
 
 
 class MaxRect:
     def __init__(self, w, h):
         self.width=w
         self.height=h
-        self.rects=[Rectangle(0, 0, w, h, plot_options=MAXRECT_PLOT_OPTIONS)]
+        self.free_rects=[Rectangle(0, 0, w, h)]
 
     def plot(self):
-        plotrects(self.rects, self.width, self.height)
+        plotrects(*self.free_rects, area_w=self.width, area_h=self.height)
         
-        
-if __name__ == '__main__':
-    m=MaxRect(10,10)
-    m.plot()
+    def cut_rect(self, rect):
+        new_free_rects=[]
+        for rf in self.free_rects:
+            if rect.overlap(rf):
+                new_rects=rf.cut_rect(rect)
+                if new_rects:
+                    new_free_rects.extend(new_rects)
+            else:
+                new_free_rects.append(rf)
+        self.free_rects=new_free_rects
---
+++ Kaggle Santa/scripts/maxrect_pack/test_maxrect.py
@@ -0,0 +1,16 @@
+from maxrect_pack.maxrect import MaxRect
+from maxrect_pack.recthelper import random_rects
+
+
+WIDTH=20
+HEIGHT=20
+
+if __name__ == '__main__':
+    m=MaxRect(WIDTH,HEIGHT)
+    
+    rects_cut=random_rects(WIDTH,HEIGHT,4,4,3)
+    for r in rects_cut:
+        m.cut_rect(r)
+        
+    m.plot()
+    
